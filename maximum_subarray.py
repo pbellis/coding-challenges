51c9459218e1273d35424b9540a7ff0cfc2a3ec7
Patrick Bellis <pbellis[AT]bu.edu>
2018-07-07 16:36:16 -0400

    prepending dnc to as there is a linear time algorithm to implement

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -10,7 +10,7 @@ def naive_maximum_subarray(arr):
 				max_delta = delta
 	return bi, bj, max_delta
 
-def maximum_subarray(arr):
+def dnc_maximum_subarray(arr):
 	deltas = [y - x for x, y in zip(arr, arr[1:])]
 	lo, hi, sum = maximum_subarray_rec(deltas, 0, len(deltas) - 1)
 	return lo, hi + 1, sum
@@ -43,14 +43,14 @@ def maximum_subarray_rec(deltas, lo, hi):
 		right = maximum_subarray_rec(deltas, mid + 1, hi)
 		cross = find_max_crossing_subarray(deltas, lo, mid, hi)
 		return max((left, right, cross), key=lambda x: x[2])
-
+		
 if __name__ == "__main__":		
 	print("running tests...")
 	from random import randint
 	for _ in range(10):
 		arr = [randint(0, 99999) for _ in range(randint(100, 1000))]
 		expected = naive_maximum_subarray(arr)
-		actual = maximum_subarray(arr)
+		actual = dnc_maximum_subarray(arr)
 		if expected != actual:
 			print("expected: ", expected)
 			print("actual: ", actual)
@@ -58,4 +58,4 @@ if __name__ == "__main__":
 	from timeit import Timer
 	arr = [randint(0, 99999) for _ in range(1000)]
 	print('naive: ', Timer(lambda: naive_maximum_subarray(arr)).timeit(100))
-	print('d&c: ', Timer(lambda: maximum_subarray(arr)).timeit(100))
+	print('d&c: ', Timer(lambda: dnc_maximum_subarray(arr)).timeit(100))
